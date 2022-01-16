77f9b70943052c9c798d1bf9bd069ef8e6c862ac
Dipti Shegar <65717483+diptishegar@users.noreply.github.com>
2022-01-16 16:34:28 +0530

    Update Home.js

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { useParams } from "react-router-dom";
 import Loader from "../Components/Loader";
 import ProductCard from "../Components/ProductCard";
 
