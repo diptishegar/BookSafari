00b6c54516dc16fea55e5002967f6ec7a670cedf
Dipti Shegar <65717483+diptishegar@users.noreply.github.com>
2022-01-16 16:39:59 +0530

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,7 +5,7 @@ function Header(){
     return(
         <header style={{ margin:"0rem 0rem 0rem -.3rem" }} className="mb-3 ml-0 border-b bg-gray-100 rounded-lg shadow w-full border-purple-300 p-3 flex justify-between items-center">
             <span className="pb-3 pt-3 text-purple-600 font-mono font-medium text-3xl px-3">
-            Whatta product!
+            BookWorld
             </span>
            <Navigation />
         </header>
