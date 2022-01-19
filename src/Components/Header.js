c5d400281bc32f59b77f50d7a090765687a7fb25
soydiptishegar <9130dipti@gmail.com>
2022-01-20 01:28:00 +0530

    designed a little2

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,7 +5,7 @@ function Header(){
     return(
         <header style={{ margin:"0rem 0rem 0rem -.36rem" }} className="mb-3 ml-0 border-b bg-gray-100 rounded-lg shadow w-full border-purple-300 p-3 flex justify-between items-center">
             <span className="pb-3 pt-3 text-gray-900 font-mono font-medium text-3xl px-3">
-            BookWorld
+            BookSafari
             </span>
            <Navigation />
         </header>
--- src/Components/NavigationMenu.js
+++ src/Components/NavigationMenu.js
@@ -7,22 +7,22 @@ import { HashLink } from 'react-router-hash-link';
 function NavigationMenu(props) {
   return (
     <div className="mt-3 bg-white ml-2 rounded h-full">
-              <span className="pb-5 text-gray-800 pt-6 font-mono bg-white font-medium text-3xl px-3">Whatta Product!</span>
+              <span className="pb-5 text-gray-800 pt-6 font-mono bg-white font-medium text-3xl px-3">BookSafari</span>
               <ul>
                 <li>
-                  <Link onClick={props.closeMenu} to="/" className="font-mono text-xl pl-5 text-gray-700 pt-6 mt-6 py-5 rounded border-b border-gray-400 block">
+                  <Link onClick={props.closeMenu} to="/" className="flex justify-start flex-row items-center font-mono text-xl pl-5 text-gray-700 pt-6 mt-6 py-5 rounded border-b border-gray-400 block">
                   <Icon.House />
                     Home
                   </Link>
                 </li>
                 <li>
-                  <HashLink onClick={props.closeMenu} smooth to="/AboutUs" className="font-mono pl-5 text-xl text-gray-700 py-5 rounded border-b border-gray-400 block">
-                    <span className="content"><Icon.InfoLg />About</span>
+                  <HashLink onClick={props.closeMenu} smooth to="/AboutUs" className="flex justify-start flex-row items-center font-mono pl-5 text-xl text-gray-700 py-5 rounded border-b border-gray-400 block">
+                    <Icon.InfoLg />About
                   </HashLink>
                 </li>
                <li>
-                 <HashLink onClick={props.closeMenu} smooth to="/#FooterPagedown" className="font-mono text-xl pl-5 text-gray-700 py-5 rounded border-b border-gray-400 block">
-                 Contact
+                 <HashLink onClick={props.closeMenu} smooth to="/#FooterPagedown" className="flex justify-start flex-row items-center font-mono text-xl pl-5 text-gray-700 py-5 rounded border-b border-gray-400 block">
+                 <Icon.Telephone /> Contact
                  </HashLink>
                  
                </li>
