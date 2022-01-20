505da1bba7418d4b4d95beb62a6e9c80d2d45a31
soydiptishegar <9130dipti@gmail.com>
2022-01-20 12:14:11 +0530

    designed a little5

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,7 +3,7 @@ import Navigation from "./Navigation";
 
 function Header(){
     return(
-        <header style={{ margin:"0rem 0rem 0rem -.36rem" }} className="mb-3 ml-0 border-b bg-gray-100 rounded-lg shadow w-full border-purple-300 p-3 flex justify-between items-center">
+        <header style={{ margin:"0rem 0rem 0rem -.36rem" }} className="mb-3 ml-0 border-b bg-white rounded-lg shadow w-full p-3 flex justify-between items-center">
             <span className="pb-3 pt-3 text-gray-900 font-mono font-medium text-3xl px-3">
             BookSafari
             </span>
